d3521e214018ab9b501000c851c819e306e288a8
Максим Щапов <maksimsapov0456@gmail.com>
2024-10-16 17:34:06 +0500

    Task5

---
+++ Tyuiu.SchcapovMA.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.SchcapovMA.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.SchcapovMA.Sprint2.Task5.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Спринт #2 | Выполнил: Щапов М.А. | ИБКСб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Щапов Максим Александрович | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* Составить программу, которая в зависимости от порядкового номера дня    *");
+            Console.WriteLine("* недели(1, …,7)выводит на экран его название(понедельник, …,воскресенье).*");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            Console.WriteLine("* Введите номер дня недели:                                               *");
+
+            int numDay = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+            if ((numDay < 1)|| (numDay > 7)) { res = "Введено неверное значение!"; }
+            else
+            {
+                res = "Это день недели: " + ds.FindDayName(numDay);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
